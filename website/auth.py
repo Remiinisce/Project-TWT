b3c2b54b07f128a00411abfe2b2c8ae8065b74bc
Remiinisce <shaisibrahim@gmail.com>
2022-10-28 11:04:16 +0100

    Update. Ran into Firstname Error - looking into it.

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,46 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .views import views
+from . import db
+from . import models
+from flask_sqlalchemy import SQLAlchemy
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import TheWishlistTreeDB
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    data = request.form
+    print(data)
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
     
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be great than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be atleast 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            return redirect(url_for(views.home))
+
+    
     return render_template("register.html")
